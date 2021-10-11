a3c45d2bfc6efc0d9c45ef6dee8147a083929024
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-10-11 15:09:51 -0300

    style: 💄 indent

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,24 +1,21 @@
 import C3 from "../../assets/img/i01_Mouse Gamer C3 TECH.jpg";
 import GFire from "../../assets/img/GFIRE.jpg";
 import G503 from "../../assets/img/G503.jpg";
-import GTB from '../../assets/img/xzone gtb-01.png'
-import BrightKey  from '../../assets/img/0541 Bright.jpg'
-import Klim from '../../assets/img/Klim Chroma.jpg'
-import P60 from '../../assets/img/p60.jpg'
-import Nemesis from '../../assets/img/nm-837 nemesis.jpg'
-import RedDragon from '../../assets/img/redragon seiryu.jpg'
-import Gshield from '../../assets/img/Gshield.jpg'
-import G3 from '../../assets/img/G320B5V2.jpg'
-import HG01 from '../../assets/img/hg01.jpg'
+import GTB from "../../assets/img/xzone gtb-01.png";
+import BrightKey from "../../assets/img/0541 Bright.jpg";
+import Klim from "../../assets/img/Klim Chroma.jpg";
+import P60 from "../../assets/img/p60.jpg";
+import Nemesis from "../../assets/img/nm-837 nemesis.jpg";
+import RedDragon from "../../assets/img/redragon seiryu.jpg";
+import Gshield from "../../assets/img/Gshield.jpg";
+import G3 from "../../assets/img/G320B5V2.jpg";
+import HG01 from "../../assets/img/hg01.jpg";
 
 export const Products = {
-
-  Mouses:[
-    
+  Mouses: [
     { name: "Mouse Gamer C3 TECH", price: 200, img: C3, id: "01" },
-  { name: "Mouse óptico Gamer G-fire", price: 250, img: GFire, id: "02" },
-  { name: "Mouse Gamer G503", price: 320, img: G503, id: "03" },
-
+    { name: "Mouse óptico Gamer G-fire", price: 250, img: GFire, id: "02" },
+    { name: "Mouse Gamer G503", price: 320, img: G503, id: "03" },
   ],
 
   Keyboards: [
@@ -28,21 +25,13 @@ export const Products = {
   ],
 
   MousePads: [
-
-
     { name: "Mouse Pad P60", price: 85, img: P60, id: "07" },
     { name: "Mouse Pad Nemesis", price: 106, img: Nemesis, id: "08" },
     { name: "Mouse Pad RedDragon", price: 210, img: RedDragon, id: "09" },
   ],
   Headphones: [
-
-
     { name: "Headphone Gshield", price: 105, img: Gshield, id: "10" },
     { name: "Headphone G320B5V2", price: 106, img: G3, id: "11" },
     { name: "Headphone HG01", price: 210, img: HG01, id: "12" },
   ],
-}
-  
-
-
-
+};
